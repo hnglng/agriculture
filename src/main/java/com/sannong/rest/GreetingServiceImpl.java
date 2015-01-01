0f0fa6279b7770b8482d2652b8f539a897d513ad
hnglng <hnglng@live.com>
2015-01-02 00:29:54 +0800

    Add Jersey RESTful service framework.

---
+++ src/main/java/com/sannong/rest/GreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sannong.rest;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Bright Huang on 1/2/15.
+ */
+@Component
+public class GreetingServiceImpl implements GreetingService{
+    @Override
+    public String greet(String who) {
+        return String.format("hello, %s!", who);
+    }
+}
